a0c5a2e91335f08454bdf0c3fd2f631d722e06ae
Leonardo Bisaro <leonardo.bisaro@gmail.com>
2026-05-13 18:58:00 -0300

    Update GuitarDIListViewComponent.h

--- Source/GuitarDIListViewComponent.h
+++ Source/GuitarDIListViewComponent.h
@@ -86,7 +86,6 @@ public:
 private:
     AppSettings& appSettings;
 
-    // juce::TextButton backButton; // removed per design
     PlusIconButton recordNewButton;
     
     juce::Viewport viewport;
